6518340f4a88eb004b5431144dc022bad7ae8ecd
Vytautas Kontrimas <vytautas.kontrimas@outlook.com>
2020-08-31 23:28:17 +0100

    Move Axial to Axial.cs

--- Assets/Code/Map/Grid.cs
+++ Assets/Code/Map/Grid.cs
@@ -29,47 +29,6 @@ namespace Simple4X
     public struct Axial
     {
         public int q, r;
-
-        public Axial(int q, int r)
-        {
-            this.q = q;
-            this.r = r;
-        }
-
-        public static Axial Round(float q, float r) {
-            float x = q;
-            float z = r;
-            float y = -x - z;
-
-            int rx = Mathf.RoundToInt(x);
-            int ry = Mathf.RoundToInt(y);
-            int rz = Mathf.RoundToInt(z);
-
-            float xdiff = Mathf.Abs(rx - x);
-            float ydiff = Mathf.Abs(ry - y);
-            float zdiff = Mathf.Abs(rz - z);
-
-            if (xdiff > ydiff && xdiff > zdiff) {
-                rx = -ry - rz;
-            }
-            else if (ydiff > zdiff) {
-                ry = -rx - rz;
-            }
-            else {
-                rz = -rx - ry;
-            }
-
-            return new Axial(rx, rz);
-        }
-
-        public static explicit operator Axial(Offset offset)
-        {
-            int q = offset.col - (offset.row - (offset.row & 1)) / 2;
-            int r = offset.row;
-            return new Axial(q, r);
-        }
-    }
-
     // TODO: Rename this to TileMap. Then we can have separate TileMap, InfluenceMap, PopulationMap, etc... classes 
     public class Grid : MonoBehaviour
     {
