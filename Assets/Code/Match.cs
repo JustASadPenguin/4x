019bfcbab4866fcf8182ec62bf07bc3e3ea52b59
Michael Paterson <mikro.paterson@gmail.com>
2020-08-31 18:30:08 +0100

    Added influence

---
+++ Assets/Code/Match.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Match : MonoBehaviour
+{
+    [SerializeField] Company[] companies = new Company[1];
+}
--- Assets/Code/TileComponent.cs
+++ Assets/Code/TileComponent.cs
@@ -3,6 +3,7 @@
 namespace Simple4X {
     public class TileComponent : MonoBehaviour {
         [SerializeField] Tile type_;
+        [SerializeField] int[] influence = new int[1];
 
         public Tile Type {
             get { return type_; }
