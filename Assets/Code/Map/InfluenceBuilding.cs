5d1e743dc39676f63b52bc9dcf21ea24d47100a8
Vytautas Kontrimas <vytautas.kontrimas@outlook.com>
2020-09-01 01:52:20 +0100

    Subtract influence

--- Assets/Code/Map/InfluenceBuilding.cs
+++ Assets/Code/Map/InfluenceBuilding.cs
@@ -12,10 +12,7 @@ namespace Simple4X
         public override void SetUpTile(Grid grid, Axial position, Transform root)
         {
             base.SetUpTile(grid, position, root);
-            Debug.Log("lel");
 
-            //iterate over neighbours
-            // TODO: Add Neighbours() to axial: foreach (neighbour in position.Neighbours())
             Queue<Axial> toVisit = new Queue<Axial>();
             foreach (Axial neighbour in position.GetNeighbours()) {
                 toVisit.Enqueue(neighbour);
@@ -32,6 +29,35 @@ namespace Simple4X
 
                 if (grid.IsWithinBounds(neighbour)) {
                     grid.playerInfluence[neighbour.q][neighbour.r] += Mathf.FloorToInt(centerInfluence * (1.0f - Axial.Distance(neighbour, position) / (float)radius));
+                }
+
+                foreach(Axial next in neighbour.GetNeighbours()) {
+                    if (Axial.Distance(next, position) < radius){
+                        toVisit.Enqueue(next);
+                    }
+                }
+            }
+        }
+
+        public override void RemoveTile(Grid grid, Axial position, Transform root) {
+            base.RemoveTile(grid, position, root);
+
+            Queue<Axial> toVisit = new Queue<Axial>();
+            foreach (Axial neighbour in position.GetNeighbours()) {
+                toVisit.Enqueue(neighbour);
+            }
+
+            HashSet<Axial> visited = new HashSet<Axial>();
+            while (toVisit.Count > 0) {
+                Axial neighbour = toVisit.Dequeue();
+                if (visited.Contains(neighbour)) {
+                    continue;
+                }
+
+                visited.Add(neighbour);
+
+                if (grid.IsWithinBounds(neighbour)) {
+                    grid.playerInfluence[neighbour.q][neighbour.r] -= Mathf.FloorToInt(centerInfluence * (1.0f - Axial.Distance(neighbour, position) / (float)radius));
                 }
 
                 foreach(Axial next in neighbour.GetNeighbours()) {
